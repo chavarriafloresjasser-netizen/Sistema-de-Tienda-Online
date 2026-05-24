cdf7229a2da69384513c8f33a299e39c59bd9d19
Jasser David Chavarria Flores <chavarriafloresjasser@gmail.com>
2026-05-23 21:01:08 -0600

    Se añadio la biblioteca de Nugget Microsoft.SQL.Client para la conección futura a una base de datos que se encuentra en desarrollo, y la fase beta de la conección de la base de datos al prograna

---
+++ Sistema de Tienda Online/DataBaseConnectionShop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data;
+class DataBaseConnetionShop
+{
+    //Dirección de coneccion de la base de datos
+    private string connectionString = "";
+    //Coneccion con la base de datos
+    private SqlConnection connect;
+    //Metodo de conección de la base de datos
+    private void Connect()
+    {
+        connect = new SqlConnection(connectionString);
+    }
+    //Constructor
+    public DataBaseConnetionShop()
+    {
+        Connect();
+    }
+}
