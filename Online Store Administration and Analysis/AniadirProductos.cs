401bcbb56a9691c403634277fc6747359e44aa37
Jasser David Chavarria Flores <chavarriafloresjasser@gmail.com>
2026-05-29 19:21:56 -0600

    Se hizo una reestructuración por completo de todo el sistema de la tienda online, así que porfavor lea detenidamente todos los cambios realizados.
    1- Se creo un nuevo proyecto llamado Online Store Administration and Analysis, osea que es un código completamente diferente e independiente del primer proyecto (Sistema de Tienda Online) en el cual se ha ido trabajando inicialmente con siguientes añadidos:
    
        a) Se agrego la clase AniadirProducto en el cual en el futuro hará que podamos añadir productos en la tienda online. Si recuerdan bien, esta clase existia en el proyecto inicial, pero se traslado esa clase al nuevo proyecto con fines de que esté todo ordenado
    
       b) Se añadio la clase Diseño y la clase Interface en la cual, Diseño hace lo mismo que la clase DiseñosGenerales del primer proyecto creado; La clase Intarface es la interfaz que va a ver el encargado de llevar el control de los productos, ventas, analisis.
    
         c) Se creo una carpeta en especifica para la conección con la futura base de datos que se está creando
    
    2- Se reestructuró todo el sistema del primer proyecto creado en el cual se realizaron los siguientes cambios:
    
        a) Se eliminaron dos clases del sistema que fueron AniadirProducto debido a su incorporación en el nuevo proyecto y a su vez porque no es logico que el usuario pueda  añadir productos, y ManejoDeIds debido a que todo eso se va a manejar desde la base de datos futura
    
        b) Es lo mismo que se hizo en el punto 1.C para al proyecto ya existente
    
        c) Se creo una clase llamada UserInterface en el cual se crea la interfaz de usuario y por ende el traslado completo del codigo llevado a program para llevarlo a este nueva clase
    
                  c.1 - Se creo un nuevo método llamado IniciarSesionEnCuenta que por el momento solo va a quedar declarado para despúes conectarlo a la base de datos futura.
    
    3 - Se añadieron comentarios a varios metodos del primer proyecto creado

--- Online Store Administration and Analysis/AniadirProductos.cs
+++ Online Store Administration and Analysis/AniadirProductos.cs
@@ -1,4 +1,4 @@
-﻿public class AniadirProducto
+﻿public class AniadirProductos
 {
     private string? _nombreProducto;
     private string? _descripcion;
---
+++ Sistema de Tienda Online/UserInterface.cs
@@ -0,0 +1,124 @@
+﻿public static class UserInterface
+{ 
+    /// <summary>
+    /// Inicio de todo el programa, donde le pide al usuario que inicie sesion
+    /// si ya posee una, de lo contrario que cree una nueva
+    /// </summary>
+    public static void Inicio()
+    {
+        DiseñosGenerales diseñosGenerales = new DiseñosGenerales();
+        diseñosGenerales.RecuadroPrincipal("Glados Shoping");
+        bool excepcion = false;
+        //Frontend
+        Console.WriteLine("Sea bienvenido al sistema de tienda online");
+        diseñosGenerales.RemarcarTexto("¿Tiene una cuenta ya registrada?");
+        diseñosGenerales.Si_o_No();
+        string? opcion;
+        do
+        {
+            do
+            {
+                opcion = Console.ReadLine();
+
+                if (opcion == null)
+                {
+                    Console.WriteLine("No puede quedar nulo el campo");
+                    excepcion = false;
+                }
+                excepcion = true;
+            } while (excepcion == false);
+
+            opcion = opcion.Trim().ToUpper();
+
+            if (opcion != "SI" && opcion != "NO")
+            {
+                Console.WriteLine("Opción no válida, por favor ingrese 'Si' o 'No'");
+                excepcion = false;
+            }
+        } while (excepcion == false);
+
+        switch (opcion)
+        {
+            case "SI":
+                IniciarSesionEnCuenta();
+                break;
+            case "NO":
+                CrearNuevaCuentaDeUsuario();
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Realiza el proceso de creación de una nueva cuenta de usuario,
+    /// en el cual todos los campos son obligatorios, si dado caso el
+    /// usuario ingresa un dato no válido, se le indicará el error y 
+    /// se le solicitará que vuelva a ingresar la información hasta que sea correcta.
+    /// </summary>
+    public static void CrearNuevaCuentaDeUsuario()
+    {
+        DiseñosGenerales diseñosGenerales = new DiseñosGenerales();
+        diseñosGenerales.RecuadroPrincipal("Registrar nueva cuenta");
+        CrearCuentaUsuario? nuevoUsuario = null;
+        bool opcion = false;
+        string? PrimerNombre = "";
+        string? SegundoNombre = "";
+        string? PrimerApellido = "";
+        string? SegundoApellido = "";
+        string? CorreoElectronico = "";
+        string? Contraseña = "";
+        int Telefono = 0;
+        do
+        {
+            diseñosGenerales.RemarcarTexto("Primer Nombre:");
+            PrimerNombre = Console.ReadLine();
+            diseñosGenerales.RemarcarTexto("Segundo Nombre:");
+            SegundoNombre = Console.ReadLine();
+            diseñosGenerales.RemarcarTexto("Primer Apellido:");
+            PrimerApellido = Console.ReadLine();
+            diseñosGenerales.RemarcarTexto("Segundo Apellido:");
+            SegundoApellido = Console.ReadLine();
+            diseñosGenerales.RemarcarTexto("Correo Electrónico:");
+            CorreoElectronico = Console.ReadLine();
+            diseñosGenerales.RemarcarTexto("Contraseña:");
+            Contraseña = Console.ReadLine();
+            diseñosGenerales.RemarcarTexto("Teléfono:");
+            do
+            {
+                try
+                {
+                    Telefono = Convert.ToInt32(Console.ReadLine());
+                    opcion = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Por favor ingrese un número válido para el teléfono.");
+                    opcion = false;
+                }
+            } while (opcion == false);
+
+            try
+            {
+                nuevoUsuario = new CrearCuentaUsuario(PrimerNombre, CorreoElectronico, Contraseña, Telefono, SegundoNombre, PrimerApellido, SegundoApellido);
+                opcion = true;
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                opcion = false;
+            }
+        } while (opcion == false);
+    }
+
+    /// <summary>
+    /// CLase hecha para que el usuario que ya posea una cuenta
+    /// que pueda iniciar sesión, en el cual se le solicitará 
+    /// su correo electrónico y contraseña para poder acceder a 
+    /// dicha cuenta
+    /// </summary>
+    public static void IniciarSesionEnCuenta()
+    {
+        DiseñosGenerales diseñosGenerales = new DiseñosGenerales();
+        diseñosGenerales.RecuadroPrincipal("Iniciar sesión");
+        // Lógica para iniciar sesión
+    }
+}
